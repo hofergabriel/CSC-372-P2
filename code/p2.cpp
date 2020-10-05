c1ebd31df2d5754d06dfa9d54eb45f9204132d3d
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-10-05 00:50:56 -0500

    it compiles!

--- code/p2.cpp
+++ code/p2.cpp
@@ -7,23 +7,34 @@ Course: CSC-372
 #include <iostream>
 #include <vector>
 #include <map>
-#define inf 1e9
+#include <cmath>
 using namespace std;
 
-void readv(int n);
+vector<int> read_v(int n);
 vector<int> prefix(vector<int> &v);
-void brute(vector<int> &pv, int loc, int row, int bw);
+int brute(vector<int> &pv, int loc, int row, int bw, int mxR);
 map<int,map<int,int>> dp;
+int get_mxR(vector<int> &v, int bw);
+map<int,pair<int,int>> ans;
+void display();
 
 /********************************************************************
 *********************************************************************/
 int main(){
-  int bw, n;
+  int bw, n, mxR;
+  vector<int> v, pv;
   cin>>bw>>n; // read board width & number of announcements 
 
-  v=readv(n) // read announcements
+  v=read_v(n); // read announcements
   pv=prefix(v); // make prefix vector
-  brute(v); // solve naively??
+
+  mxR = get_mxR(v, bw);
+  cout<<"mxR: "<<mxR<<endl;
+
+  brute(pv, 0, 0, bw, mxR); // solve naively??
+
+  // answer should be in dp[0][0]
+
 
   return 0;
 }
@@ -33,33 +44,67 @@ Solution shoudl be in dp[row=0][loc=0] ...
 @param mnC - min cost
 @param mnLoc - location/index where mnC is found
 *********************************************************************/
-void brute(vector<int> &pv, int loc, int row, int bw){
-  if(row>mxR) return inf; 
-  if(loc==v.size()) return 0;
-  for(int loc2=loc, mnC=1e9, mnLoc, t; pv[loc2]-(loc==0 ? 0:pv[loc-1])<=bw; loc2++){
+int brute(vector<int> &pv, int loc, int row, int bw, int mxR){
+  if(row>mxR) return ((int)1<30); 
+  if(loc==pv.size()) return 0;
+  int mnC=1<<30, mnLoc, loc2=loc, t;
+  // if(dp.count(row) && dp[row].count(loc)) return dp[row][loc];
+  for(; pv[loc2]-(loc==0 ? 0:pv[loc-1])<=bw; loc2++){
     t = 
       pow(bw-(pv[loc2]-(loc==0 ? 0:pv[loc-1])),3) + 
-      brute(pv,loc2+1,row+1,bw); // need to account for loc-1<0
+      brute(pv,loc2+1,row+1,bw,mxR); // need to account for loc-1<0
     if(t<mnC){ mnC=t; mnLoc=loc2; }
   }
+  ans[row]={loc,loc2}; 
   return dp[row][loc]=mnC;
 }
 
 /********************************************************************
 *********************************************************************/
+int get_mxR(vector<int> &v, int bw){
+  int r=0;
+  for(int i=0, s=0; i<v.size(); i++){
+    s+=v[i]; 
+    if(s>bw){ r++; s=v[i]; }
+  }
+  return r;
+}
+
+
+/********************************************************************
+*********************************************************************/
 vector<int> prefix(vector<int> &v){
   vector<int> pv=v;
-  for(int i=1;i<n;i++) pv[i]+=v[i-1];
+  for(int i=1;i<v.size();i++) pv[i]+=v[i-1];
   return pv;
 }
 
 /********************************************************************
 *********************************************************************/
-void readv(int n){
+vector<int> read_v(int n){
   vector<int> v(n);
   for(int i=0;i<n;i++) cin>>v[i];
   return v;
 }
+
+/********************************************************************
+*********************************************************************/
+void display(){
+  cout<<dp[0][0]<<endl;
+  for(auto e:ans)
+    cout<<"("<<(e.second).first<<", "<<(e.second).second<<") --> "<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
