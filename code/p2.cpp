12e2402e3aaa7bed8fc92d957e706af390464be2
Gabriel Hofer <leirbagabriel468@gmail.com>
2020-10-05 01:49:39 -0500

    ok

--- code/p2.cpp
+++ code/p2.cpp
@@ -27,15 +27,20 @@ int main(){
 
   v=read_v(n); // read announcements
   pv=prefix(v); // make prefix vector
+  cout<<"pv: ";
+  for(int i=0;i<pv.size();i++){
+    cout << pv[i] << ' ';
+  }
+  cout<<endl;
+
+
 
   mxR = get_mxR(v, bw);
   cout<<"mxR: "<<mxR<<endl;
 
-  brute(pv, 0, 0, bw, mxR); // solve naively??
-
+  int mnC = brute(pv, 0, 0, bw, mxR); // solve naively??
+  cout<<"mnC: "<<mnC<<endl;
   // answer should be in dp[0][0]
-
-
   return 0;
 }
 
@@ -49,11 +54,19 @@ int brute(vector<int> &pv, int loc, int row, int bw, int mxR){
   if(loc==pv.size()) return 0;
   int mnC=1<<30, mnLoc, loc2=loc, t;
   // if(dp.count(row) && dp[row].count(loc)) return dp[row][loc];
-  for(; pv[loc2]-(loc==0 ? 0:pv[loc-1])<=bw; loc2++){
+  
+  for(; loc2<(pv.size()-1) && pv[loc2]-(loc==0 ? 0:pv[loc-1])<=bw; loc2++){
+
+    cout<<"\t\t\t\trow: "<<row<<" loc: "<<loc<<" bw: "<<bw<<endl;
+    cout<<"cost: "<<pow(bw-(pv[loc2]-(loc==0 ? 0:pv[loc-1])),3)<<endl;
+    cout<<"NEXT: "<< pv[loc2+1]-(loc==0 ? 0:pv[loc-1]) <<endl;
+    cout<<"pv.size()-1: "<<pv.size()-1<<endl;
+    
     t = 
       pow(bw-(pv[loc2]-(loc==0 ? 0:pv[loc-1])),3) + 
       brute(pv,loc2+1,row+1,bw,mxR); // need to account for loc-1<0
     if(t<mnC){ mnC=t; mnLoc=loc2; }
+
   }
   ans[row]={loc,loc2}; 
   return dp[row][loc]=mnC;
@@ -70,12 +83,11 @@ int get_mxR(vector<int> &v, int bw){
   return r;
 }
 
-
 /********************************************************************
 *********************************************************************/
 vector<int> prefix(vector<int> &v){
   vector<int> pv=v;
-  for(int i=1;i<v.size();i++) pv[i]+=v[i-1];
+  for(int i=1;i<v.size();i++) pv[i]+=pv[i-1];
   return pv;
 }
 
